5b84e0d90eb5acdca111c5f21479263e84d23bbf
LeaYeh <smile2140@gmail.com>
2025-04-25 16:53:39 +0000

    feat: Implement cross entropy loss function

---
+++ litetorch/loss/cross_entropy.py
@@ -0,0 +1,17 @@
+"""
+litetorch/loss/cross_entropy.py
+This module defines the Cross Entropy loss function for a neural network framework.
+This loss function is commonly used for multi-class classification tasks.
+It calculates the loss between the predicted probabilities and the true class labels.
+
+Author: Lea Yeh
+Version: 0.0.1
+Date: 2025-04-25
+"""
+
+import numpy as np
+from litetorch.loss.base import Loss
+
+
+class CrossEntropyLoss(Loss):
+    pass

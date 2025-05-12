bfa1334c62e9bfe4a087bd2d416e7168ff7717a3
LeaYeh <smile2140@gmail.com>
2025-05-12 13:12:17 +0000

    fix: Reduce the broadcase shape in backward for the sub function

--- src/litetorch/core/sub_function.py
+++ src/litetorch/core/sub_function.py
@@ -10,6 +10,7 @@ Date: 2025-04-26
 from typing import Tuple
 from litetorch.core.function import Function
 from litetorch.core.tensor import Tensor
+from litetorch.utils.linear_algebra import reduce_broadcast_shape
 
 
 class SubFunction(Function):
@@ -23,14 +24,10 @@ class SubFunction(Function):
 
     def forward(self, a: Tensor, b: Tensor) -> Tensor:
         """
-        Forward pass of the addition operation.
-
-        Parameters:
-        - a: First input tensor.
-        - b: Second input tensor.
-
+        Forward pass of the subtraction operation.
+        ...
         Returns:
-        - Output tensor after addition.
+            Output tensor after subtraction.
         """
         return Tensor(a.data - b.data, requires_grad=a.auto_grad or b.auto_grad)
 
@@ -49,4 +46,16 @@ class SubFunction(Function):
         # The gradient of the sum is 1 for both inputs
         # This means that the gradient of the output with respect to each input is 1.
         # In the case of subtraction, the gradient with respect to b is -1.
-        return grad_outputs[0], -1 * grad_outputs[0]
+        grad_output = grad_outputs[0].data
+        a = self.inputs[0]
+        b = self.inputs[1]
+
+        grad_a = grad_output
+        grad_b = -grad_output
+
+        if grad_a.shape != a.shape:
+            grad_a = reduce_broadcast_shape(grad_a, a.shape)
+        if grad_b.shape != b.shape:
+            grad_b = reduce_broadcast_shape(grad_b, b.shape)
+
+        return Tensor(grad_a, requires_grad=a.auto_grad), Tensor(grad_b, requires_grad=b.auto_grad)

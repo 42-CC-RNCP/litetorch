6a415e325e9aa7c7e06251d901210dce4f04b58b
Lea Yeh <lea.yeh.ml@gmail.com>
2025-05-18 22:15:18 +0000

    chore: Simplfy the import path of metrics

---
+++ src/litetorch/metrics/__init__.py
@@ -0,0 +1,12 @@
+from .accuracy import TrainAccuracy, ValAccuracy
+from .loss import TrainLoss, ValLoss
+from .visualization import ConfusionMatrixImage
+
+
+__all__ = [
+    "TrainAccuracy",
+    "ValAccuracy",
+    "TrainLoss",
+    "ValLoss",
+    "ConfusionMatrixImage",
+]

5aa31551ea51cb50bc001d5d375c189812f50389
Lea Yeh <lea.yeh.ml@gmail.com>
2025-05-29 12:12:25 +0000

    feat: Add min delta detect in early stop

--- src/litetorch/training/callbacks/early_stop_callback.py
+++ src/litetorch/training/callbacks/early_stop_callback.py
@@ -5,18 +5,20 @@ class EarlyStopCallback(Callback):
     """
     Early stopping callback.
     """
-    def __init__(self, patience: int = 10, monitor: str = 'val_loss', mode: str = 'min'):
+    def __init__(self, patience: int = 10, monitor: str = 'val_loss', mode: str = 'min', min_delta: float = 0.0):
         """
         Args:
             patience (int): Number of epochs with no improvement after which training will be stopped.
             monitor (str): Metric to monitor. Default is 'val_loss'.
             mode (str): One of {'min', 'max'}. In 'min' mode, training will stop when the quantity monitored has stopped decreasing.
                         In 'max' mode, it will stop when the quantity monitored has stopped increasing.
+            min_delta (float): Minimum change in the monitored quantity to qualify as an improvement.
         """
         super().__init__()
         self.patience = patience
         self.monitor = monitor
         self.mode = mode
+        self.min_delta = min_delta
         self.best_score = float('inf') if mode == 'min' else float('-inf')
         self.early_stop = False
         
@@ -25,7 +27,8 @@ class EarlyStopCallback(Callback):
         
     def on_epoch_end(self, trainer):
         current = getattr(trainer, self.monitor)[-1]  # Get the last value of the monitored metric
-        improved = (current < self.best_score) if self.mode == "min" else (current > self.best_score)
+        diff = current - self.best_score
+        improved = (diff < -self.min_delta) if self.mode == 'min' else (diff > self.min_delta)
         
         if self.best_score is None or improved:
             self.best_score = current
